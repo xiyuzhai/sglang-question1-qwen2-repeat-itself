269efb8b37e3138d6f48c31981bb87d086642b4a
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-15 23:41:27 -0800

    freeze

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ source .venv/bin/activate
 pip install --upgrade pip
 pip install torch torchvision torchaudio
 pip install "sglang[all]" --find-links https://flashinfer.ai/whl/cu124/torch2.4/flashinfer/
+pip install -r requirements.txt
